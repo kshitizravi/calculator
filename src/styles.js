7f18626078174ae0742236cbddc35ea52e900662
Kshitiz Ravi <kshitiz.ravi@gmail.com>
2021-01-08 12:57:01 +0530

    commit

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+const React = require('react-native');
+const { StyleSheet } = React;
+
+export default {
+
+    container: {
+        flex:1,
+        flexDirection: 'column'
+    },
+
+    contHistory:{
+        flex: 0.3,
+        backgroundColor: 'red'
+    },
+}
