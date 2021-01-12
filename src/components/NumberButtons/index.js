b9d450b46a684b5db1faf7700797fec9dd47ac42
Kshitiz Ravi <kshitiz.ravi@gmail.com>
2021-01-12 13:14:41 +0530

    Commit

--- src/components/NumberButtons/index.js
+++ src/components/NumberButtons/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { render } from 'react-dom';
 import {
     View,
     Text,
@@ -8,5 +9,38 @@ import {
 import styles from './styles';
 
 export default class NumberButtons extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return false;
+    }
 
+_handleOnPress = (value) => {
+    requestAnimationFrame( () => {
+        this.props.onBtnPress(value);
+    });
+}
+
+render() {
+    return (
+        <View style={styles.container}>
+                {
+                    this.props.buttons.map((row, index) => (
+                        <View key={index} style={styles.contRow}>
+                            { 
+                                row.map((col,index) => (
+                                    <TouchableNativeFeedback
+                                        key={index}
+                                        onPress={() => this._handleOnPress(col)}
+                                        background={TouchableNativeFeedback.SelectableBackground()}>
+                                        <View style={styles.contButton}>
+                                            <Text style={styles.txtDefault}>{col}</Text>
+                                        </View>
+                                    </TouchableNativeFeedback>
+                                ))
+                            }
+                        </View>
+                    ))
+                }
+            </View>
+        );
+    }
 }
