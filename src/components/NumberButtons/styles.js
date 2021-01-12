555961b458b368eccb625d9f1b408bd1449d88b5
Kshitiz Ravi <kshitiz.ravi@gmail.com>
2021-01-12 12:54:54 +0530

    commit on master

---
+++ src/components/NumberButtons/styles.js
@@ -0,0 +1,42 @@
+const React = require('react-native');
+const {StyleSheet } = React;
+
+export default {
+    container: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+
+    contHistory:{
+        flex: 0.3,
+        backgroungColor: 'red'
+    },
+
+    contHistory:{
+        flex: 0.3,
+        backgroungColor: 'red'
+    },
+
+    contOutput: {
+        flex: 0.2,
+    },
+
+    contButtons:{
+        flex: 0.5,
+        backgroungColor: '#bdc3c7'
+    },
+
+    PlaceHolderOutput: {
+        flex: 1,
+        backgroungColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        paddingRight: 20
+    },
+
+    txtDefault: {
+        color: '#fff',
+        fontFamily: 'Helvetica-Light',
+        fontSize: 30
+    }
+};
